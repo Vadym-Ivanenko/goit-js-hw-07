cdf646ec0a2af56352e2ca55be092319e807754b
Vadym <v.ivanenko000@gmail.com>
2023-01-27 21:31:59 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function createGallery(items) {
+  return items
+    .map((item) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+                </a>
+        </div>
+`;
+    })
+    .join("");
+}
+galleryContainer.innerHTML = createGallery(galleryItems);
+
+galleryContainer.addEventListener("click", showBigImg);
+
+function showBigImg(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createGallery(items) {
+  return items
+    .map((item) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+                </a>
+        </div>
+`;
+    })
+    .join("");
+}
+galleryContainer.innerHTML = createGallery(galleryItems);
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
